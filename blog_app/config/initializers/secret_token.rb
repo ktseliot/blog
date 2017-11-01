439360b264b2d90f9e9a30174b162cf764781f6e
koflynn@spartaglobal.com <Katie@LAPTOP-U76C1JQI.localdomain>
2017-11-01 10:27:13 +0000

    Initial commit

---
+++ blog_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogApp::Application.config.secret_token = '101a06938418466cfacfebc6391108adaa3d0d3ddd3e36be397c086609d1279182f66b08c77d4bad021cf1ed7aa359e514e6fdee3809bc549a42f3d211ad549d'
